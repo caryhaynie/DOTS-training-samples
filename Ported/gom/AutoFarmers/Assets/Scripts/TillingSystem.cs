4664c82218c64c55568259c8036210790f2768ef
Jack Pritz <jackpr@unity3d.com>
2019-10-31 13:11:28 -0700

    Complete first pass of tilling. Clean up RockSmashSystem

--- Ported/gom/AutoFarmers/Assets/Scripts/TillingSystem.cs
+++ Ported/gom/AutoFarmers/Assets/Scripts/TillingSystem.cs
@@ -1,5 +1,6 @@
 using Unity.Entities;
 using Unity.Jobs;
+using Unity.Collections;
 using Unity.Mathematics;
 using Unity.Transforms;
 
@@ -17,6 +18,45 @@ public class TillingSystem : JobComponentSystem
             ECB.SetComponent<Translation>(jobIndex, tilled, new Translation { Value = new float3(0.25f, 0.5f, 0.25f) });
 
             ECB.SetComponent<LandState>(jobIndex, entity, new LandState { Value = LandStateType.Tilled });
+            ECB.RemoveComponent<NeedsTilling>(jobIndex, entity);
+        }
+    }
+
+    [RequireComponentTag(typeof(TillGroundIntention))]
+    [ExcludeComponent(typeof(PathElement))]
+    struct BuildTillArray : IJobForEachWithEntity<TargetEntity>
+    {
+        public NativeArray<Entity> GroundToMarkToTill;
+        public EntityCommandBuffer.Concurrent EntityCommandBuffer;
+
+        public void Execute(Entity entity, int index, ref TargetEntity target)
+        {
+            if (target.Value == Entity.Null)
+            {
+                EntityCommandBuffer.RemoveComponent<TillGroundIntention>(index, entity);
+                EntityCommandBuffer.RemoveComponent<TargetEntity>(index, entity);
+                EntityCommandBuffer.AddComponent<NeedGoal>(index, entity);
+            }
+            else
+                GroundToMarkToTill[index] = target.Value;
+        }
+    }
+
+    // Don't Burst because ground can be tilled by multiple farmers, and we don't want write collisions
+    struct MarkToTill : IJob
+    {
+        [ReadOnly]
+        [DeallocateOnJobCompletion]
+        public NativeArray<Entity> GroundToMarkToTill;
+        
+        public EntityCommandBuffer.Concurrent EntityCommandBuffer;
+
+        public void Execute()
+        {
+            foreach (var entity in GroundToMarkToTill)
+            {
+                EntityCommandBuffer.AddComponent<NeedsTilling>(0, entity);
+            }
         }
     }
 
@@ -29,11 +69,30 @@ public class TillingSystem : JobComponentSystem
 
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        var job = new MarkTilesAsTilledJob {
+        var ToMarkToTillQuery = GetEntityQuery(new EntityQueryDesc {
+            None = new ComponentType[] { typeof(PathElement) },
+            All = new ComponentType[] { typeof(TillGroundIntention), typeof(TargetEntity) }
+        });
+        
+        NativeArray<Entity> EntityArray = new NativeArray<Entity>(ToMarkToTillQuery.CalculateEntityCount(), Allocator.TempJob);
+
+        var MarkTilled = new MarkTilesAsTilledJob {
             ECB = m_ECBSystem.CreateCommandBuffer().ToConcurrent(),
             TilledPrefab = GetSingleton<PrefabManager>().TilledPrefab
         }.Schedule(this, inputDeps);
-        m_ECBSystem.AddJobHandleForProducer(job);
-        return job;
+
+        var BuildArrayJob = new BuildTillArray
+        {
+            EntityCommandBuffer = m_ECBSystem.CreateCommandBuffer().ToConcurrent(),
+            GroundToMarkToTill = EntityArray
+        }.Schedule(this, MarkTilled);
+
+        var MarkToTillJob = new MarkToTill 
+        {
+            EntityCommandBuffer = m_ECBSystem.CreateCommandBuffer().ToConcurrent()
+        }.Schedule(BuildArrayJob);
+
+        m_ECBSystem.AddJobHandleForProducer(MarkToTillJob);
+        return MarkToTillJob;
     }
 }
