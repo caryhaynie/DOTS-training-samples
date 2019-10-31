ca2f683aaa61f9387643722d137d24c7eea76fd7
rebeccab <rebeccaad@Unitys-MacBook-Pro-7.local>
2019-10-31 16:07:34 -0700

    sell plant system

---
+++ Ported/gom/AutoFarmers/Assets/Scripts/SellPlantSystem.cs
@@ -0,0 +1,54 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+[UpdateInGroup(typeof(SimulateFarmGroup))]
+public class SellPlantSystem : JobComponentSystem
+{
+    EndSimulationEntityCommandBufferSystem m_EntityCommandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        m_EntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    [BurstCompile]
+    [ExcludeComponent(typeof(PathIndex))]
+    [RequireComponentTag(typeof(SellPlantIntention))]
+    struct SellPlantJob : IJobForEachWithEntity<TargetEntity>
+    {
+        public EntityCommandBuffer.Concurrent EntityCommandBuffer;
+
+        public void Execute(Entity entity,
+            int index,
+            ref TargetEntity target)
+        {
+
+            EntityCommandBuffer.RemoveComponent<SellPlantIntention>(index, entity);
+            EntityCommandBuffer.RemoveComponent<TargetEntity>(index, entity);
+
+            EntityCommandBuffer.AddComponent<NeedGoal>(index, entity);
+
+            // do we need to un-parent plant to the farmer?
+
+            EntityCommandBuffer.DestroyEntity(0, target.Value); // is target the store or a plant?
+
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        var job = new SellPlantJob
+        {
+            EntityCommandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent()
+        }.Schedule(this, inputDependencies);
+
+        m_EntityCommandBufferSystem.AddJobHandleForProducer(job);
+
+        return job;
+    }
+}
